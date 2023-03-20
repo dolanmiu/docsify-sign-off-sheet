c8aee09e2070b5eca947308ae36cf5b8b6661076
Dolan Miu <dolan_miu@hotmail.com>
2023-03-20 23:38:14 +0000

    Minor tweaks

--- src/container-block.ts
+++ src/container-block.ts
@@ -2,7 +2,7 @@ export const createContainerBlock = (elements: HTMLElement[]) => {
   // <div class="flex flex-col items-end gap-1">...</div>
 
   const output = document.createElement("div");
-  output.classList.add("flex", "flex-col", "items-end", "gap-2");
+  output.classList.add("flex", "flex-col", "items-end", "gap-2", "pt-5");
   elements.forEach((element) => {
     output.appendChild(element);
   });
--- src/main.ts
+++ src/main.ts
@@ -11,55 +11,51 @@ import { createContainerBlock } from "./container-block";
 declare var $docsify: any;
 
 (function () {
-  (window as any).$docsify.plugins = [
-    (hook: any, _vm: any) => {
-      let signOffCadence = "6 months";
-      hook.beforeEach(function (content: string) {
-        $docsify.formatUpdated = (time: number) => {
-          const date = moment(new Date(time));
+  const myPlugin = (hook: any, _vm: any) => {
+    let signOffCadence = "6 months";
+    hook.beforeEach(function (content: string) {
+      (window as any).$docsify.formatUpdated = (time: number) => {
+        const date = moment(new Date(time));
 
-          return date.format("MMM Do YYYY");
-        };
+        return date.format("MMM Do YYYY");
+      };
 
-        const [, match] = content.match(/sign-off-cadence:([a-z0-9 ]+)/i) ?? [];
+      const [, match] = content.match(/sign-off-cadence:([a-z0-9 ]+)/i) ?? [];
 
-        signOffCadence = match ? match.trim() : "6 months";
+      signOffCadence = match ? match.trim() : "6 months";
+    });
+
+    hook.doneEach(function () {
+      const mainContent = document.getElementById("main");
+
+      if (!mainContent) {
+        return;
+      }
+
+      const modal = createModal(document, [...findSignOffSheet(document.body)]);
+
+      const tableBodyRows = modal.querySelectorAll("tbody > tr");
+
+      if (tableBodyRows.length === 0) {
+        return;
+      }
+
+      const lastSignOffDate = findLatestDate(tableBodyRows);
+
+      mainContent.prepend(
+        createContainerBlock([
+          createSignOffStatusElement(lastSignOffDate, signOffCadence),
+          modal,
+        ])
+      );
+
+      const tables = mainContent.querySelectorAll("table");
+      tables.forEach((table) => {
+        table.setAttribute("style", "display: table;");
       });
-
-      hook.doneEach(function () {
-        const mainContent = document.getElementById("main");
-
-        if (!mainContent) {
-          return;
-        }
-
-        const modal = createModal(document, [
-          ...findSignOffSheet(document.body),
-        ]);
-
-        const tableBodyRows = modal.querySelectorAll("tbody > tr");
-
-        if (tableBodyRows.length === 0) {
-          return;
-        }
-
-        const lastSignOffDate = findLatestDate(tableBodyRows);
-
-        mainContent.prepend(
-          createContainerBlock([
-            createSignOffStatusElement(lastSignOffDate, signOffCadence),
-            modal,
-          ])
-        );
-
-        const tables = mainContent.querySelectorAll("table");
-        tables.forEach((table) => {
-          table.setAttribute("style", "display: table;");
-        });
-      });
-    },
-    ...($docsify.plugins ? [$docsify.plugins] : []),
-  ];
+    });
+  };
+  // Add plugin to docsify's plugin array
+  (window as any).$docsify = (window as any).$docsify || {};
+  (window as any).$docsify.plugins = [].concat((window as any).$docsify.plugins || [], myPlugin as any);
 })();
-
-export {};
